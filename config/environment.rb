a7d43bb714848225c5f12b1ef43a46776a307deb
adrianeric <adrianeric40@gmail.com>
2015-06-14 23:39:33 -0300

    modificacion archivo enviroment.rb para modificar inflector al español

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# modificacion de inflector para español
+ActiveSupport::Inflector.inflections do |inflect|
+    inflect.plural /([^djlnrs])([A-Z]|_|$)/, '\1s\2'
+    inflect.plural /([djlnrs])([A-Z]|_|$)/, '\1es\2'
+    inflect.plural /(.*)z([A-Z]|_|$)$/i, '\1ces\2'
+    inflect.singular /([^djlnrs])s([A-Z]|_|$)/, '\1\2'
+    inflect.singular /([djlnrs])es([A-Z]|_|$)/, '\1\2'
+    inflect.singular /(.*)ces([A-Z]|_|$)$/i, '\1z\2'
+    inflect.irregular 'user', 'users'
+    inflect.irregular 'admin', 'admins'
+    inflect.irregular 'account', 'accounts'
+    inflect.irregular 'password', 'passwords'
+    inflect.irregular 'session', 'sessions'
+    inflect.irregular 'ud', 'uds'
+end
